3d4c76ab044abba08d6bf057277e24fdd8849a24
Mayur Jain <mayur@Mayurs-MacBook-Air.local>
2023-01-15 21:37:30 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Header from "./Components/Header";
+import {Todos} from "./Components/Todos";
+import {Footer} from "./Components/Footer";
+// import  from "./Components/";
 
 function App() {
+
+  const onDelete = (todo) => {
+    console.log("On Delete Function.", todo);
+  }
+
+  let todos = [
+    {
+      sno: 1,
+      title: "Go to the market",
+      description: "Need to get groceries."
+    },
+    {
+      sno: 2,
+      title: "Go to the mall",
+      description: "Need to get wedding dress."
+    },
+    {
+      sno: 3,
+      title: "Go to the treasure island.",
+      description: "Enjoy the rides."
+    },
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header title="Todos List" searchBar={false} />
+      <Todos todos={todos} onDelete={onDelete}/>
+      <Footer/>
+    </>
   );
 }
 
 export default App;
+ 
